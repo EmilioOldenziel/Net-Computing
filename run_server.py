aab43dc5cd422c2ae4c50b2d0b2bd9ec82edaba7
Yannick Stoffers <y.stoffers@rug.nl>
2017-03-29 19:16:12 +0200

    run_server.py is safe now.

--- run_server.py
+++ run_server.py
@@ -6,13 +6,5 @@ def IP ():
 	return socket.gethostbyname(socket.gethostname())
 
 if __name__ == '__main__':
-	uri, daemon, bcast_server = Pyro4.naming.startNS (
-		host = IP (), 
-		port = 0,
-		enableBroadcast = False)
-		# bcport = 0
-		# )
-	print ("Name server started at {0}".format (uri))
-
 	app.debug = True
 	app.run (host = IP ())
