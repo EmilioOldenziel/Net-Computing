044d99406e9206dc253e1f216a56d7a7e72c2a4e
martijnluinstra <martijnluinstra@gmail.com>
2017-04-03 03:44:23 +0200

    Also play sound on windows

--- actuator.py
+++ actuator.py
@@ -1,9 +1,15 @@
 import os
+import sys
 import socket
 import Pyro4        # RMI
 
+if sys.platform.startswith('win32'):
+    import winsound
+
+
 def IP ():
 	return socket.gethostbyname(socket.gethostname())
+
 
 @Pyro4.expose                               # Expose this class to RMI
 @Pyro4.behavior(instance_mode="single")     # Single instance per node
@@ -13,7 +19,10 @@ class Actuator:
 		os.system ("shutdown -h now")
 
 	def play_sound (self):
-		os.system ("mplayer 0477.wav")
+		if sys.platform.startswith('win32'):
+		    winsound.PlaySound('0477.wav', winsound.SND_FILENAME)
+		else:
+			os.system ('mplayer 0477.wav')
 
 	# Starts the pyro daemon
 	def start (self):
@@ -25,9 +34,11 @@ class Actuator:
 			ns = True
 			)
 
+
 def main ():
 	actuator = Actuator ()
 	actuator.start ()
+
 
 if __name__ == "__main__":
 	main ()
