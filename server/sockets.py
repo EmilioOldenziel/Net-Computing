dd78d2c219a1b49d7c1db44be828dd0ca730b7b2
martijnluinstra <martijnluinstra@gmail.com>
2017-04-03 03:14:29 +0200

    Initiate context before using Flask features in sockets

--- server/sockets.py
+++ server/sockets.py
@@ -4,30 +4,20 @@ import json
 
 from geventwebsocket import WebSocketApplication
 
-from . import db
+from . import app, db
 from .models import Measurement
 
 class MeasurementsApplication(WebSocketApplication):
     def on_message(self, message):
         if message is None:
             return
+
+        app.app_context()
         
         message = json.loads(message)
 
         if message['msg_type'] == 'measurements':
             self.process_measurements(message['data'])
-
-    # def send_client_list(self, message):
-    #     current_client = self.ws.handler.active_client
-    #     current_client.nickname = message['nickname']
-
-    #     self.ws.send(json.dumps({
-    #         'msg_type': 'update_clients',
-    #         'clients': [
-    #             getattr(client, 'nickname', 'anonymous')
-    #             for client in self.ws.handler.server.clients.values()
-    #         ]
-    #     }))
 
     def broadcast(self, data):
         for client in self.ws.handler.server.clients.values():
